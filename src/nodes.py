3963a60c7ce387816a11370d3d666c5ba678a28b
yin9w9 <cyliu901@gmail.com>
2026-05-19 13:33:01 +0800

    msp430 blink_led sucess

--- src/nodes.py
+++ src/nodes.py
@@ -87,7 +87,7 @@ def get_model() -> ChatOpenAI:
         openai_api_key=api_key,
         openai_api_base=MODEL_API_BASE,
         temperature=MODEL_TEMPERATURE,
-        max_tokens=1024,  #>256
+        max_tokens=512,  #>256
     ) 
 
 
